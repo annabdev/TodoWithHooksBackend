fdf28cad2505a792725f8c1ff4cbeecc418be7ca
Anna Beagley <beagley.dev@gmail.com>
2019-05-16 10:21:28 -0600

    updated monk

--- server.js
+++ server.js
@@ -1,51 +1,67 @@
 const express = require("express");
-const app = express();
-const PORT = 4200;
-const monk = require("monk");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+//  same as import express from "express"
+const app = express()
+const port = 4000
+const monk = require('monk')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
 
-const url = require("./keys").mongoURI;
-
+// Connection URL
+const url = 'mongodb://annabdev:psswrd@cluster0-shard-00-00-dv5uy.mongodb.net:27017,cluster0-shard-00-01-dv5uy.mongodb.net:27017,cluster0-shard-00-02-dv5uy.mongodb.net:27017/HOOKTODO?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
+//change mongodb connection to 2.5 version
 
 const db = monk(url);
 
+
 db.then(() => {
-  console.log("Connected to MongoDB");
-});
-const people = db.get("work");
-
-
-app.listen(PORT, function() {
-    console.log("Server is running on Port: " + PORT);
-})
-const todo = app.get("work")
-
-app.use(cors());
-app.use(bodyParser.json());
-
-app.get("/", async (req, res) => {
-  const result = await todo.find()
-  console.log("get called");
-  res.status(200).send(result);
+    console.log('Connected correctly to server')
 })
 
-app.post("/", async (req, res) => {
-  const result = await todo.insert(req.body)
-  console.log("post worked")
-  res.status(200).send(result)
-})
+//extantiate middleware
+app.use(cors())
+app.use(bodyParser.json())
 
-app.put("/:id", async (req, res) => {
-  const result = await todo.findOneAndUpdate(req.params.id, req.body)
-  console.log("put called")
-  res.status(200).send(result)
-})
 
-app.delete("/:id", async (req, res) => {
-  const result = await todo.findOneAndDelete(req.params.id)
-  console.log("delete called")
-  res.status(200).send(result)
-});
+const Todos = db.get('backend')
+//const '<database>' = db.get('<collection>')
 
+app.get('/', (req, res) => {
+    const result = Todos.find()
+    console.log("get")
+    res.status(200)
+        //200 means it worked
+        .send(result)
+    })
+    //.find grabs everything in the database and sends it
+
+
+app.post('/', async (req, res) => {
+        const result = await Todos.insert(req.body)
+        console.log("Post")
+        res.status(200).send(result)
+    })
+
+app.put('/:id', async (req, res) => {
+        const result = await Todos.findOneAndUpdate(req.body, req.params.id)
+        console.log("Put")
+        res.status(200)
+            //200 means it worked
+            .send(result)
+    })
+
+app.delete('/:id', async (req, res) => {
+        const result = await Todos.findOneAndDelete(req.params.id)
+        console.log("delete")
+        res.status(200)
+            //200 means it worked
+            .send(result)
+    })
+
+//the request is literally the fetch
+
+
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
